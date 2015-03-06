77dcc0892f6aa3c41b0f63d968b40ddc1d856832
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2015-03-06 17:32:42 +0100

    Added removeAllListeners and code format

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var Emitter = require('emitter-component'),
-    debounce = require('debounce');
+var Emitter = require('emitter-component');
+var debounce = require('debounce');
 
 var resize = module.exports = {
     applyResize: function() {
         this.width = window.innerWidth;
         this.height = window.innerHeight;
-        this.halfWidth = 0.5*this.width;
-        this.halfHeight = 0.5*this.height;
+        this.halfWidth = 0.5 * this.width;
+        this.halfHeight = 0.5 * this.height;
         this.emit('resize');
     },
     addListener: function(listener) {
@@ -16,6 +16,9 @@ var resize = module.exports = {
     },
     removeListener: function(listener) {
         if(listener) this.off('resize', listener);
+    },
+    removeAllListeners: function() {
+        this.off('resize');
     }
 };
 
