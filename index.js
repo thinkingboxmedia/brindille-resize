44d1683ac0bcc9e35df2d00ed2d2bc7fc7a4fd12
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-05-12 12:10:23 +0200

    add tests and travis

--- index.js
+++ index.js
@@ -3,38 +3,49 @@
 var Emitter = require('component-emitter');
 var debounce = require('debounce');
 
-var resize = module.exports = {
-  onGlobalResize: function() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.halfWidth = 0.5 * this.width;
-    this.halfHeight = 0.5 * this.height;
+var resize = module.exports;
 
-    this.applyResize();
-    this.debouncedResize();
-  },
-  applyResizeDebounce: function() {
-    this.emit('resizeDebounce');
-  },
-  applyResize: function() {
-    this.emit('resize');
-  },
-  addListener: function(listener, noDebounce) {
-    this.on(noDebounce ? 'resize' : 'resizeDebounce', listener);
-  },
-  removeListener: function(listener) {
-    if (listener) {
-        this.off('resize', listener);
-        this.off('resizeDebounce', listener);
-    }
-  },
-  removeAllListeners: function() {
-    this.off();
+// Make resize an event emitter
+Emitter(resize);
+
+resize.debounceDelay = 50;
+
+resize.onGlobalResize = function() {
+  this.width = window.innerWidth;
+  this.height = window.innerHeight;
+  this.halfWidth = 0.5 * this.width;
+  this.halfHeight = 0.5 * this.height;
+
+  this.applyResize();
+  if (this.debounceDelay > 0) {
+    this.debounceResize();
+  } else {
+    this.applyResizeDebounce();
   }
 };
 
-Emitter(resize);
-resize.debouncedResize = debounce(resize.applyResizeDebounce.bind(resize), 50);
+resize.applyResizeDebounce = function() {
+  this.emit('resizeDebounce');
+};
+
+resize.applyResize = function() {
+  this.emit('resize');
+};
+
+resize.addListener = function(listener, noDebounce) {
+  this.on(noDebounce ? 'resize' : 'resizeDebounce', listener);
+};
+
+resize.removeListener = function(listener) {
+  if (listener) {
+    this.off('resize', listener);
+    this.off('resizeDebounce', listener);
+  }
+};
+
+resize.debounceResize = debounce(resize.applyResizeDebounce.bind(resize), resize.debounceDelay);
+
+// first call to init values
 resize.onGlobalResize();
 
 window.addEventListener('resize', resize.onGlobalResize.bind(resize));
